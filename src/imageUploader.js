28fb5c3c03db16c2d7a9a61e5cf5da1fad9e35ed
Marcelo Espinoza <marcelo>
2024-03-04 07:44:45 -0300

    Added input validations to image uploader

--- src/imageUploader.js
+++ src/imageUploader.js
@@ -1,10 +1,13 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useState, useRef } from 'react';
 import './style.css';
 import { storage, textDB } from './Firebase/firebase';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { collection } from 'firebase/firestore'
 import { v4 } from "uuid";
 import { addDoc, getDoc, getDocs } from '@firebase/firestore';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 export function ImageUploader() {
 
@@ -12,121 +15,60 @@ export function ImageUploader() {
     const [progress, setProgress] = React.useState(0);
     const [previewImage, setPreviewImage] = React.useState(0);
     const [imageUpload, setImageUpload] = React.useState(null);
-    const [imgSrc, setImgSrc] = useState('')
     const [isLoading, setIsLoading] = React.useState(false);
     const [showImg, setShowImg] = useState(false);
-    const [fields, setFields] = useState({});
-    const [errors, setErrors] = useState({});
+    const [celular, setCelular] = React.useState('');
+    const [correo, setCorreo] = React.useState('');
+    const [nroPatente, setNroPatente] = React.useState('');
+    const [showLoader, setShowLoader] = React.useState('');
+    const formRef = useRef(null);
 
-    const handleChange = (field, value) => {
-      setFields({
-        ...fields,
-        [field]: value
-      })
-    }
-
-    const uploadImage = () => {
+    async function uploadImage() {
         if (imageUpload == null) {
           
         } else {
         const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-        uploadBytes(imageRef, imageUpload).then(data => {
+        return await uploadBytes(imageRef, imageUpload).then(data => {
           getDownloadURL(data.ref).then(val =>{
             const valRef = collection(textDB, 'patentes')
-            addDoc(valRef, {nro_patente:'PROBANDO', celular:'celu', correo:'correo', imgUrl: val})
+            addDoc(valRef, {nro_patente:nroPatente,
+                            provincia:"Formosa",
+                            fecha:new Date().toLocaleString(),
+                            celular:celular, 
+                            correo:correo, 
+                            imgUrl: val})
           })
         })
        }
-      }
-        
-      const handleSubmit = () => {
-        setIsLoading(true);
-        Tesseract.recognize(image, 'eng', {
-          logger: (m) => {
-            console.log(m);
-            if (m.status === 'recognizing text') {
-              setProgress(parseInt(m.progress * 100));
-            } else {
-              console.log(m);
-            }
-          },
-        })
-          .catch((err) => {
-            console.error(err);
-          })
-          .then((result) => {
-            console.log(result.data);
-            setText(result.data.text);
-            setIsLoading(false);
-          });
+      }             
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        const isFormValid = formRef.current.checkValidity();    
+        if (isFormValid) {
+          setShowLoader(true)
+          const result = uploadImage()
+          setShowLoader(false)
+          console.log('Form is valid! Submitting...'+result);
+          setShowImg(!showImg)
+          formRef.current.reset();
+        } else {
+          console.log('Form is not valid. Please check your inputs.');
+        }
       };
 
-      const handleValidation = () => {
-        const formFields = {...fields};
-        const formErrors = {};
-        let formIsValid = true;
-    
-        //Name
-        if(!formFields["celular"]){
-          formIsValid = false;
-          formErrors["celular"] = "Cannot be empty";
-        }
-    
-        if(typeof formFields["celular"] !== "undefined"){
-          if(!formFields["celular"].match(/^[a-zA-Z]+$/)){
-            formIsValid = false;
-            formErrors["celular"] = "Only letters";
-          }       
-        }
-    
-        //Email
-        if(!formFields["correo"]){
-          formIsValid = false;
-          formErrors["correo"] = "Cannot be empty";
-        }
-    
-        if(typeof formFields["correo"] !== "undefined"){
-          let lastAtPos = formFields["correo"].lastIndexOf('@');
-          let lastDotPos = formFields["correo"].lastIndexOf('.');
-    
-          if (!(lastAtPos < lastDotPos && lastAtPos > 0 && formFields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
-            formIsValid = false;
-            formFields["correo"] = "Email is not valid";
-          }
-        }     
-  
-        setErrors(formErrors)
-        return formIsValid;
-      }
-
-      const formSubmit = (e) => {
-        e.preventDefault();
-        if(handleValidation()){
-          alert("Form submitted");
-        }else{
-          alert("Form has errors.")
-        }
-      }
-
     return (
-      <form onSubmit={e => formSubmit(e)}>
-
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={showLoader}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
         <div className="container" style={{ height: '100vh' , width:'100%'}}>
         <div className="col-md-5 mx-auto d-flex flex-column text-center">
-          {(
-            <h3 className="fw-bold mb-3 h2">SUBI LA IMAGEN DE LA PATENTE</h3>
-          )}
-          { isLoading &&(
-            <>
-              <progress className="form-control" value={progress} max="100">
-                {progress}%{' '}
-              </progress>{' '}
-              <p className="text-center py-0 my-0">Converting:- {progress} %</p>
-            </>
-          )}
-          {(
-            <>
+          <h3 className="fw-bold mb-3 h2">SUBI LA IMAGEN DE LA PATENTE</h3>
               <input
+                required
                 type="file"
                 id='selectFile'
                 accept="image/*"
@@ -138,36 +80,30 @@ export function ImageUploader() {
                 }}
                 className="form-control mt-5 mb-2"
               />
-              {showImg && <img id="previewImage" src={previewImage} alt="image"/>}
-              <input
-                type="button"
-                hidden={true}
-                onClick={handleSubmit}
-                className="btn btn-primary mt-5"
-                value="Escanear imagen"
-              />
-            </>
-          )}
-          {(
-            <>
-            <div className="col-md-6 mx-auto text-center">
-                <input type="text" className="form-control text-center"  placeholder="Numero patente"/>
+              {showImg && <img id="previewImage" src={previewImage} alt="image"/>}            
+              <>
+              <div className="col-md-6 mx-auto text-center">
+                <input required type="text" pattern="[A-Za-z][A-Za-z]\d\d\d[A-Za-z][A-Za-z]" className="form-control text-center" onChange={(e) => { setNroPatente(e.target.value) }} placeholder="Formato XX123XX"/>
               <hr />
-                <input type="text" name="celular"  className="form-control text-center" placeholder="Celular" onChange={e => handleChange('celular', e.target.value)} value={fields["celular"] || ''}/>
-                <span className="error">{errors["celular"]}</span>
+                <input  required type="text" 
+                        name="celular" 
+                        pattern="[0-9]{10}" 
+                        className="form-control text-center" 
+                        onChange={(e) => { setCelular(e.target.value) }} 
+                        placeholder="Celular"
+                        onInvalid={e => e.target.setCustomValidity('Mínimo 10 caracteres')}
+                        onInput={e => e.target.setCustomValidity("")}
+                  />
               <hr />
-                <input type="email" name="correo"  className="form-control text-center" placeholder="Correo" onChange={e => handleChange('correo', e.target.value)} value={fields["correo"] || ''} />
-                <span className="error">{errors["correo"]}</span>
+                <input required type="email" name="correo"  className="form-control text-center" onChange={(e) => { setCorreo(e.target.value) }} placeholder="Correo"  />
               </div>
               <input
-              id="submit"
-                type="button"
-                onClick={uploadImage}
+                id="submit"
+                type="submit"
                 className="btn btn-primary mt-5"
                 value="Guardar"
               />
             </>
-          )}
         </div>
         </div>
        </form> 
